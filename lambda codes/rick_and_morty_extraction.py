bccb27ab2d503d541a4fb3eb5ae08ecdc97f4cba
Joy Achieng Ogutu <103546150+Ogutu01@users.noreply.github.com>
2024-09-02 16:31:37 +0300

    Edited the names of some variables in the extraction.py file

--- lambda codes/rick_and_morty_extraction.py
+++ lambda codes/rick_and_morty_extraction.py
@@ -1,50 +1,72 @@
+import pandas as pd
 import requests
 import json
-import pandas as pd
 import datetime
 import s3_file_operations as s3_ops
 
-def extract_data(api_url, table_name):
-    page = 1
-    next = True
-    all_data = []
-
-    while next:
-        print(f"Extracting page {page} Data from {table_name}.....")
-        response = requests.get(f"{api_url}?page={str(page)}")
-        data = response.json().get('results', [])
-        all_data.extend(data)
-
-        if response.json().get('info', {}).get("next") is not None:
-            page += 1
-        else:
-            break
-
-    return pd.DataFrame(all_data)
-
-def save_to_s3(df, bucket, table_name):
-
-    s3_ops.write_data_to_s3(df,
-                            bucket_name=bucket,
-                            key=f"Rick&Morty/Untransformed/{table_name}.csv")
-
 def lambda_handler(event, context):
-    print("Starting Extraction....")
-    bucket = "de-masterclass" # s3 bucket name
-
-    tables = {
-        "Character": "https://rickandmortyapi.com/api/character",
-        "Location": "https://rickandmortyapi.com/api/location",
-        "Episode": "https://rickandmortyapi.com/api/episode"
+    # TODO implement
+    endpoints = ["character", "episode", "location"]
+    
+    data_dict = {
+        "character": [],
+        "episode": [],
+        "location": []
+        }
+    for endpoint in endpoints:
+        print(f"\nExtracting data from {endpoint} endpoint.")
+    
+        page = 1
+    
+        next = True
+    
+        while next:
+    
+            print(f"Extracting page {page} data...")
+    
+            #Fetch data from the current page of the endpoint
+            r = requests.get(f"https://rickandmortyapi.com/api/{endpoint}/?page={str(page)}")
+            data = r.json().get("results", [])
+    
+            # Add the data to the corresponding list in the dictionary
+            data_dict[endpoint].extend(data)
+    
+            #Checking if the value of the next url is a null or None, so we stop the loop
+            if r.json().get("info", {}).get("next") is not None:
+                page += 1
+            else:
+                break
+        
+        print(f"\nFinished extracting data from {endpoint} endpoint. Total records: {len(data_dict[endpoint])}")
+    
+    # Converting the data lists to DataFrames
+    char_df = pd.DataFrame(data_dict["character"])
+    loc_df = pd.DataFrame(data_dict["location"])
+    ep_df = pd.DataFrame(data_dict["episode"])
+    
+    print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print(f"\nAll data fully extracted.\nCharacter structure: {char_df.shape},\nLocation structure: {loc_df.shape},\nEpisode structure: {ep_df.shape}")
+    
+    #Saving the data to s3
+    # Dictionary mapping DataFrame names to their corresponding keys in S3
+    dataframes = {
+        "characters.csv": char_df,
+        "locations.csv": loc_df,
+        "episodes.csv": ep_df
     }
-
-    for table_name, api_url in tables.items():
-        df = extract_data(api_url, table_name)
-        save_to_s3(df, bucket, table_name)
-        print(f"Data for {table_name} successfully saved in S3.. you can go check it out...")
-
-
+    
+    # S3 bucket name
+    bucket = "de-masterclass-ogutu"
+    
+    # Looping through each DataFrame and uploading it to S3
+    for filename, dataframe in dataframes.items():
+        key = f"Rick&Morty/Untransformed/{filename}"
+        s3_ops.write_data_to_s3(dataframe, bucket_name=bucket, key=key)
+        print(f"{filename} uploaded successfully to {key}!")
+        
+    print("\nData successfuly saved in s3, you can go check it out!")
+    
     return {
         'statusCode': 200,
-        'body': json.dumps('Success!')
+        'body': json.dumps('Hello from Lambda!')
     }
--- lambda codes/rick_and_morty_transformation.py
+++ lambda codes/rick_and_morty_transformation.py
@@ -6,21 +6,21 @@ from io import StringIO
 import s3_file_operations as s3_ops
 
 def lambda_handler(event, context):
-    bucket = "de-masterclass"  # S3 bucket name
+    bucket = "de-masterclass-ogutu"  # S3 bucket name
 
     # Read data from S3
     print("Reading Character data from S3...")
-    characters_df = s3_ops.read_csv_from_s3(bucket, 'Rick&Morty/Untransformed/Character.csv')
-    print(f"Characters DataFrame shape: {characters_df.shape}")
-    print("Reading Episode data from S3...")
-    episodes_df = s3_ops.read_csv_from_s3(bucket, 'Rick&Morty/Untransformed/Episode.csv')
-    print(f"Episodes DataFrame shape: {episodes_df.shape}")
-    print("Reading Location data from S3...")
-    location_df = s3_ops.read_csv_from_s3(bucket, 'Rick&Morty/Untransformed/Location.csv')
-    print(f"Locations DataFrame shape: {location_df.shape}")
+    char_df = s3_ops.read_csv_from_s3(bucket, 'Rick&Morty/Untransformed/characters.csv')
+    print(f"Characters DataFrame shape: {char_df.shape}")
+    print("\nReading Episode data from S3...")
+    ep_df = s3_ops.read_csv_from_s3(bucket, 'Rick&Morty/Untransformed/episodes.csv')
+    print(f"Episodes DataFrame shape: {ep_df.shape}")
+    print("\nReading Location data from S3...")
+    loc_df = s3_ops.read_csv_from_s3(bucket, 'Rick&Morty/Untransformed/locations.csv')
+    print(f"Locations DataFrame shape: {loc_df.shape}")
 
     # Check if data is loaded successfully
-    if characters_df is None or episodes_df is None or location_df is None:
+    if char_df is None or ep_df is None or loc_df is None:
         print("Error in loading data from S3")
         return {
             'statusCode': 500,
@@ -32,20 +32,20 @@ def lambda_handler(event, context):
     # Characters DataFrame transformation
     extract_id = lambda x: x.split('/')[-1] if x else None
 
-    characters_df['origin_id'] = [
+    char_df['origin_id'] = [
         extract_id(ast.literal_eval(record)['url']) if isinstance(record, str) else None
-        for record in characters_df['origin']
+        for record in char_df['origin']
     ]
 
-    characters_df['location_id'] = [
+    char_df['location_id'] = [
         extract_id(ast.literal_eval(record)['url']) if isinstance(record, str) else None
-        for record in characters_df['location']
+        for record in char_df['location']
     ]
 
-    characters_df = characters_df.drop(columns=['origin', 'location', 'episode'])
+    char_df = char_df.drop(columns=['origin', 'location', 'episode'])
 
     # Appearance DataFrame Creation
-    appearance_df = episodes_df.copy()
+    appearance_df = ep_df.copy()
 
     character_func = lambda x: [url.split('/')[-1] for url in ast.literal_eval(x)] if isinstance(x, str) else None
 
@@ -60,16 +60,16 @@ def lambda_handler(event, context):
     expanded_df = expanded_df[['id', 'episode_id', 'character_id']]
 
     # Episodes DataFrame transformation
-    episodes_df = episodes_df.drop("characters", axis=1)
+    ep_df = ep_df.drop("characters", axis=1)
 
     # Locations DataFrame transformation
-    location_df = location_df.drop('residents', axis=1)
+    loc_df = loc_df.drop('residents', axis=1)
 
     # Save final DataFrames to S3
-    s3_ops.write_data_to_s3(characters_df, bucket, 'Rick&Morty/Transformed/Character.csv')
-    s3_ops.write_data_to_s3(episodes_df, bucket, 'Rick&Morty/Transformed/Episode.csv')
+    s3_ops.write_data_to_s3(char_df, bucket, 'Rick&Morty/Transformed/Character.csv')
+    s3_ops.write_data_to_s3(ep_df, bucket, 'Rick&Morty/Transformed/Episode.csv')
     s3_ops.write_data_to_s3(expanded_df, bucket, 'Rick&Morty/Transformed/Appearance.csv')
-    s3_ops.write_data_to_s3(location_df, bucket, 'Rick&Morty/Transformed/Location.csv')
+    s3_ops.write_data_to_s3(loc_df, bucket, 'Rick&Morty/Transformed/Location.csv')
 
     return {
         'statusCode': 200,
